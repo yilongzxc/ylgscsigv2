7c49114352af561835703ac3847cff716311db61
yilongzxc <60287675+yilongzxc@users.noreply.github.com>
2020-03-16 19:33:59 +0800

    Update articles.py

--- articles.py
+++ articles.py
@@ -33,6 +33,9 @@ JAMA_Surgery = Sci("https://jamanetwork.com/journals/jamasurgery/currentissue", 
 Journal_of_Vascular_Surgery = Sci("https://www.journals.elsevier.com/journal-of-vascular-surgery/recent-articles", "pod-listing-header")
 World_Neurosurgery = Sci("https://www.journals.elsevier.com/world-neurosurgery-x/recent-articles", "pod-listing-header")
 Clinical_Orthopedics_and_Trauma = Sci("https://www.journals.elsevier.com/journal-of-clinical-orthopaedics-and-trauma/recent-articles", "pod-listing-header")
-
+Transplantation_Reviews = Sci("https://www.journals.elsevier.com/transplantation-reviews/recent-articles", "pod-listing-header")
+Transplantation_Reports = Sci("https://www.journals.elsevier.com/transplantation-reports/recent-articles", "pod-listing-header")
+Transplant_Immunology = Sci("https://www.journals.elsevier.com/transplant-immunology/recent-articles", "pod-listing-header")
+Journal_of_Heart_and_Lung_Transplantation = Sci("https://www.journals.elsevier.com/transplant-immunology/recent-articles", "pod-listing-header")
 # cd Desktop\python\python proj\yl sci
 # ylsci-env\Scripts\activate.bat
